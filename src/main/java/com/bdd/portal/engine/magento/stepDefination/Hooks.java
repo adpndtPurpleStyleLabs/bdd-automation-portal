3d398adf37bbf363ed497993b8e6dc89b58fe664
Aditya Pandit <aditya.pandit@purplestylelabs.com>
2026-07-14 16:16:09 +0530

    added text env from properties

--- src/main/java/com/bdd/portal/engine/magento/stepDefination/Hooks.java
+++ src/main/java/com/bdd/portal/engine/magento/stepDefination/Hooks.java
@@ -3,17 +3,26 @@ package com.bdd.portal.engine.magento.stepDefination;
 import com.bdd.portal.engine.DriverManager;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URL;
+import java.time.Duration;
 
 @Slf4j
 public class Hooks {
+
+    private final String environment;
+
+    public Hooks(@Value("${test.environment}") String environment) {
+        this.environment = environment;
+    }
 
     @Before
     public void setup() {
@@ -23,6 +32,18 @@ public class Hooks {
         
         try {
             WebDriver driver = null;
+            if(environment.equalsIgnoreCase("dev")){
+                ChromeOptions options =  new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("disable-notifications");
+                options.addArguments("start-maximized");
+                options.addArguments("--disable-notifications");
+                driver = WebDriverManager.chromedriver().capabilities(options).create();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                return;
+            }
+
             String gridUrlString = DriverManager.getGridUrl();
             if (gridUrlString == null || gridUrlString.isEmpty()) {
                 gridUrlString = "http://localhost:5503/wd/hub";
